317544cd942929547720ae35a06523ac7debca6a
Администратор <alexushveridze@mail.ru>
2022-03-24 22:55:35 +0300

    try to live please 2

--- alice-2-game.py
+++ alice-2-game.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request
 import logging
 import json
@@ -171,6 +173,5 @@ def get_first_name(req):
 
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
